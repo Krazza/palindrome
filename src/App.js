a01481f4bee9b910eb07fe39820fb604d3715827
Muondo Vladislav <s2200741@BCC02J6TQ6LRAir.lan>
2022-11-13 14:00:41 +0200

    palindrome taks done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+    constructor()
+    {
+        super()
+        this.PalindromeCheck = this.PalindromeCheck.bind(this);
+        this.Resultstyling = this.Resultstyling.bind(this);
+        this.state = {
+            result : "To find out if a word is a palindrome, type it in the box above",
+            palindrome : false
+        }
+    }
+
+    render()
+    {
+        return(
+            <div className="container">
+                <h1>Palindrome check</h1>
+                <input type="textarea" onChange={this.PalindromeCheck}/>
+                <this.Resultstyling />
+            </div>
+        );
+    }
+
+    Resultstyling()
+    {   
+        if(this.state.palindrome)
+            return <p className="yes">{this.state.result}</p>
+        else return <p className="no">{this.state.result}</p>
+    }
+
+    PalindromeCheck(event)
+    {
+        if(event.target.value === "")
+        {
+            this.setState({result: "To find out if a word is a palindrome, type it in the box above", palindrome : false});
+            return;
+        }
+
+        let lowRegUserInput = event.target.value.toLowerCase();
+        let reversedInput = [...lowRegUserInput].reverse().join("");
+
+        if(lowRegUserInput === reversedInput)
+            this.setState({result: "Word " + event.target.value + " is a palindrome", palindrome : true});
+        else this.setState({result: "Word " + event.target.value + " is NOT palindrome", palindrome: false});
+    }
 }
 
 export default App;
